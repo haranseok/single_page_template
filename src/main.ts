5da31b791aee31f4fb11044e5fb9e26799952f9c
haran seok <gkfks222@gmail.com>
2023-04-21 12:44:26 +0900

    vuw i18n 설치

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import Router from '@/router/index'
+import i18n from './i18n'
 
 const app = createApp(App);
 const pinia = createPinia();
 
 loadFonts()
-  app.use(pinia)
-  app.use(vuetify)
-  app.use(Router)
-  app.mount('#app')
+app.use(i18n)
+app.use(pinia)
+app.use(vuetify)
+app.use(Router)
+app.mount('#app')
